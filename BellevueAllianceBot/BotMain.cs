a11e6436f337976fe795f3cdead549676344a27c
loukylor <loukylor@gmail.com>
2022-06-16 21:45:04 -0700

    add the funni

--- BellevueAllianceBot/BotMain.cs
+++ BellevueAllianceBot/BotMain.cs
@@ -5,7 +5,9 @@ using DSharpPlus.EventArgs;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.SlashCommands.EventArgs;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BellevueAllianceBot
@@ -22,6 +24,7 @@ namespace BellevueAllianceBot
 
         public static DiscordGuild? BAGuild { get; private set; }
 
+        private static readonly Regex _imRegex = new(@"(?i:\bI'?\s*a?m\b)");
         public static void Main(string[] args)
         {
             foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
@@ -44,6 +47,24 @@ namespace BellevueAllianceBot
                 }
 
                 BAGuild = await Client.GetGuildAsync(Config.Instance.BADiscordID);
+            };
+
+            Client.MessageCreated += async (DiscordClient _, MessageCreateEventArgs ev) =>
+            {
+                if (ev.Guild != null || ev.Channel.Id != 569147890413469718 ||  ev.Author.IsBot)
+                {
+                    return;
+                }
+
+                Match match = _imRegex.Match(ev.Message.Content);
+                if (!match.Success)
+                {
+                    return;
+                }
+
+                DiscordMessageBuilder reply = new DiscordMessageBuilder().WithContent($"Hi {ev.Message.Content[(match.Index + match.Length)..].Trim()} I'm dad.");
+                reply.WithAllowedMentions(new List<IMention>());
+                await ev.Message.RespondAsync(reply);
             };
 
             SlashCommands.SlashCommandErrored += (SlashCommandsExtension _, SlashCommandErrorEventArgs e) =>
